fb2af3d4a5725b6852fcd3d0238de503897509ca
esstmrk <marko.sostaric@ericsson.com>
2020-06-14 13:27:20 +0200

    Commit on tb1.

--- src/test_project/app.java
+++ src/test_project/app.java
@@ -5,6 +5,8 @@ public class app {
 	public static void main(String[] args) {
 		
 		System.out.println("Something to print ...");
+		System.out.println("One more print ...");
+		System.out.println("This one is on tb1 ...");
 
 	}
 
